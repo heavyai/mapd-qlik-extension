e39448265fee373ff96bbf9fcd0ce7514465fe59
Marc Balaban <marc@marcbalaban.com>
2018-08-09 12:15:50 -0700

    updated filter handlers to account for more charts in qlik updated and added new tests for filter handler functions

--- src/filters-handler.js
+++ src/filters-handler.js
@@ -1,7 +1,84 @@
 import {debouncedRender} from './services/mapd-connector'
+
 let currentFilters = ""
 let fields = {}
 const ASYNC_TIMEOUT = 250
+
+// Due to contraints of Qlik letting user know whether data has a range within it.  We will assume
+// that series data will have more than 25 selections in it.  Otherwise we will treat each selection
+// as an individual filter
+const SERIES_LIMIT = 25
+
+function isFloat(n) {
+    return n === +n && n !== (n|0);
+}
+
+function isInteger(n) {
+    return n === +n && n === (n|0);
+}
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
+function isSeries(arr) {
+  var arrToCheck
+  if (!arr.length || arr.length < SERIES_LIMIT) return false
+  var firstVal = arr[0]
+
+  if (isFloat(firstVal) || isInteger(firstVal)) {
+    arrToCheck = arr
+  } else if(isValidDate(new Date(firstVal))) {
+    arrToCheck = arr.map(val => {
+      return new Date(val)
+    })
+  } else {
+    return false
+  }
+
+  // Ideally we'd like to have a delta here, but unfortunately Qlik has a random interval of dates
+  // Eg. May 3rd, May 10th, May 13th, May 20th
+  // which makes it unpredictable.
+
+  for (var i = 1; i < SERIES_LIMIT; i++) {
+    if (arrToCheck[i + 1] - arrToCheck[i] < 0) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export const uncoverDataType = (key, value) => {
+  if (key.indexOf('[') !== -1) {
+    return "HISTOGRAM"
+  } else if (isSeries(value)) {
+    return "SERIES"
+  } else {
+    return "INDIVIDUAL_SELECTIONS"
+  }
+}
+
+export const parseDataType = (type, key, values) => {
+  let parsedKey = key
+  let parsedValues = values
+
+  if (type === "HISTOGRAM") {
+    parsedKey = key.slice(key.indexOf('[') + 1, key.indexOf(']'))
+    parsedValues = parsedValues.map(value => {
+      const lowAndHigh = value.split('<')
+      return `${parsedKey} >= ${lowAndHigh[0]} and ${parsedKey} < ${lowAndHigh[2]}`
+    })
+  } else if (type === "INDIVIDUAL_SELECTIONS") {
+    parsedValues = parsedValues.map(value => {
+      return `${parsedKey}='${value}'`
+    })
+  } else if (type === "SERIES") {
+    parsedValues = [`${parsedKey} between '${parsedValues[0]}' AND '${parsedValues[parsedValues.length -1]}'`]
+  }
+
+  return {parsedValues, parsedKey}
+}
 
 export const getCurrentFilters = () => currentFilters.length ? `and (${currentFilters})` : '';
 
@@ -13,7 +90,7 @@ export function setFilterListener (qlik, layout, $element) {
     
     const currentSelectedFields = []
     
-  // This entire section is a workaround for the limitations of qSelectionThreshold being limited to 6
+    // This entire section is a workaround for the limitations of qSelectionThreshold being limited to 6
     if (selectedFields.length) {
       selectedFields.forEach( function (dimension) {
         const selectedField = dimension.qField
@@ -51,9 +128,23 @@ export function createFilters (selectedFields) {
   return filters
 }
 
+function purifyFilters (filters) {
+  return Object.keys(filters).reduce((result, key) => {
+    
+    let values = filters[key]
+    const dataType = uncoverDataType(key, values)
+    const {parsedKey, parsedValues} = parseDataType(dataType, key, values)
+
+    result[parsedKey] = parsedValues
+    return result
+  }, {})
+}
+
 export function createSQLFilterStatement (filters) {
   let SQLstatement = '';
-  const filterKeys = Object.keys(filters)
+
+  const parsedFilters = purifyFilters(filters)
+  const filterKeys = Object.keys(parsedFilters)
 
   function addToSQL(str) {
     SQLstatement = `${SQLstatement}${str}`
@@ -61,16 +152,16 @@ export function createSQLFilterStatement (filters) {
   
   filterKeys.forEach((dimension, i) => {
     if (i === 0) addToSQL(`(`)
-    filters[dimension].forEach((value, j) => {
+    parsedFilters[dimension].forEach((value, j) => {
       if (j === 0) {
         addToSQL(`(`)
       } else if (j > 0) {
         addToSQL(` OR `)
       }
-      
-      addToSQL(`${dimension}='${value}'`)
-      
-      if (j === filters[dimension].length - 1) {
+
+      addToSQL(value)
+
+      if (j === parsedFilters[dimension].length - 1) {
         addToSQL(`)`)
       }
     });
--- test/filters-handler.spec.js
+++ test/filters-handler.spec.js
@@ -1,31 +1,85 @@
 var proxyquire =  require('proxyquire').noCallThru();
-var {createFilters, createSQLFilterStatement} = proxyquire('../src/filters-handler', {
+var {
+  createFilters,
+  createSQLFilterStatement,
+  parseDataType
+} = proxyquire('../src/filters-handler', {
   './services/mapd-connector': () => {}
 });
 var assert = require('assert');
 
 describe('Create SQL Filter Statement', function () {
- it('Should not create a SQL filter statement', function () {
+    it('Should not create a SQL filter statement', function () {
         const input = {}
         assert.equal(createSQLFilterStatement(input), "");
     });
 
- it('Should create a filter statement from one column and one filter', function () {
+    it('Should create a filter statement from one column and one filter', function () {
       const input = {"column": ['filter1']}
       assert.equal(createSQLFilterStatement(input), "((column='filter1'))");
     });
 
- it('Should create a filter statement from one column and two filters', function () {
-      const input = {"column": ['filter1', 'filter2']}
-      assert.equal(createSQLFilterStatement(input), "((column='filter1' OR column='filter2'))");
+    it('Should create a filter statement from one column and three filters', function () {
+      const input = {"column": ['filter1', 'filter2', 'filter3']}
+      assert.equal(createSQLFilterStatement(input), "((column='filter1' OR column='filter2' OR column='filter3'))");
     });
 
- it('Should create a filter statement from two columns and two filters', function () {
+    it('Should create a filter statement from two columns and two filters', function () {
       const input = {
         "column": ['filter1', 'filter2'],
         "column2": ['filter3', 'filter4']
       }
       
       assert.equal(createSQLFilterStatement(input), "((column='filter1' OR column='filter2') AND (column2='filter3' OR column2='filter4'))");
+    });
+});
+
+describe('Parse Data Type Statement', function () {
+    it('Should parse HISTOGRAM data', function () {
+        var type = "HISTOGRAM"
+        var key = "=Class([amount],7500,'x',-94000)"
+        var values = [
+                        "-26500 <= x < -19000",
+                        "-19000 <= x < -11500",
+                        "-11500 <= x < -4000",
+                        "-4000 <= x < 3500"
+                      ]
+
+        var expectedResult = {
+          "parsedValues": ["amount >= -26500  and amount <  -19000","amount >= -19000  and amount <  -11500","amount >= -11500  and amount <  -4000","amount >= -4000  and amount <  3500"],
+          "parsedKey":"amount"
+        }
+
+
+        assert.equal(JSON.stringify(parseDataType(type, key, values)), JSON.stringify(expectedResult));
+    });
+
+    it('Should parse INDIVIDUAL_SELECTIONS data', function () {
+      var type = "INDIVIDUAL_SELECTIONS"
+      var key = "recipient_name"
+      var values = [
+                      "Maintenance of Way Employees",
+                      "Texas Medical Assn"
+                   ]
+
+      var expectedResult = {
+        "parsedValues":["recipient_name='Maintenance of Way Employees'","recipient_name='Texas Medical Assn'"],
+        "parsedKey":"recipient_name"
+      }
+
+      assert.equal(JSON.stringify(parseDataType(type, key, values)), JSON.stringify(expectedResult));
+    });
+
+    it('Should parse SERIES data', function () {
+      var type = "SERIES"
+      var key = "series_data"
+      var values = ["2005-01-03","2005-01-04","2005-01-05","2005-01-06","2005-01-10","2005-01-18","2005-01-21","2005-01-24","2005-01-31","2005-02-01","2005-02-07","2005-02-14","2005-02-22","2005-02-23","2005-02-24","2005-02-28","2005-03-17","2005-03-21","2005-03-28","2005-03-31","2005-04-08","2005-04-12","2005-04-15","2005-04-18","2005-04-20","2005-04-22","2005-04-26"]
+
+      var expectedResult = { 
+        parsedValues: ["series_data between \'2005-01-03\' AND \'2005-04-26\'"],
+        parsedKey: 'series_data'
+      }
+
+        assert.equal(JSON.stringify(parseDataType(type, key, values)), JSON.stringify(expectedResult));
     });
 });
